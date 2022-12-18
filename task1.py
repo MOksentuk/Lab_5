e092c91616204efff58db33cca7b1ce431d241cb
MOksentuk <113674979+MOksentuk@users.noreply.github.com>
2022-12-18 21:24:47 +0300

    Completed

--- task1.py
+++ task1.py
@@ -4,19 +4,19 @@ n = int(input())
 a = []
 
 for i in range(n):
-    a.append(random.randint(0,1))
+    a.append(random.randint(0, 1))
 
-zero = a.count(0)/len(a)
-ones = a.count(1)/len(a)
+zero = a.count(0) / len(a)  # процент нулей в списке
+ones = a.count(1) / len(a)  # процент единиц в списке
 
 c = 0
 t = 0
 
-for x in range(len(a)-1):
-    if a[c] != a[c+1]:
+for x in range(len(a) - 1):
+    if a[c] != a[c + 1]:
         t += 1
     c += 1
 
 print(a)
 print(zero, ones)
-print(1 - t/len(a))
+print(1 - t / len(a))
